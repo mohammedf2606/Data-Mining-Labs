7618bdb184bc73a1ad1a2dbf95586ecef12248d4
Sterilise <m.e.a@live.co.uk>
2022-01-24 22:33:36 +0000

    update plot

--- lab1/main.py
+++ lab1/main.py
@@ -12,10 +12,10 @@ age_expec = df.iloc[0:, 70:72]
 
 age_expec.drop(age_expec.loc[age_expec["Male life expectancy, (2012-14)"] == '.'].index, inplace=True)
 age_expec.drop(age_expec.loc[age_expec["Female life expectancy, (2012-14)"] == '.'].index, inplace=True)
+age_expec = age_expec.dropna()
 
-
-x = age_expec["Male life expectancy, (2012-14)"].values.tolist()[1:]
-y = age_expec["Female life expectancy, (2012-14)"].values.tolist()[1:]
+x = age_expec["Male life expectancy, (2012-14)"].values.tolist()
+y = age_expec["Female life expectancy, (2012-14)"].values.tolist()
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 
@@ -25,32 +25,20 @@ all_indices = list(range(len(x)))
 random_train_indices = r.sample(all_indices, train_size)
 random_test_indices = list(set(all_indices) - set(random_train_indices))
 
-print(random_train_indices)
-print(random_test_indices)
-
 index_values = list(enumerate(zip(x,y))) # Collection(index, (x,y))
 trainset = [index_values[i][1] for i in random_train_indices] #(x,y)
 testset = [index_values[i][1] for i in random_test_indices]
 
-# print(train_size)
-# print(len(testset))
-# print(len(trainset))
-
-# print(index_values)
-# print(testset)
-# print(trainset)
+##actual plot
 
 x_train, y_train = zip(*trainset)
 x_test, y_test = zip(*testset)
 
-# print("\n\n\n")
-# print(x_test)
-# print(y_test)
+fig = plt.figure()
+axes = fig.add_subplot(111)
 
-plt.scatter(x_train, y_train, s=10, c='b', marker="s", label='first')
-plt.scatter(x_test, y_test, s=10, c='r', marker="o", label='second')
+axes.scatter(x_train, y_train, s=10, c='b', marker="s", label='first')
+axes.scatter(x_test, y_test, s=10, c='r', marker="o", label='second')
 
-# plt.xlim([74, 88])
-# ax1.set_ylim([78, 92])
 plt.legend(loc='upper left')
 plt.show()
