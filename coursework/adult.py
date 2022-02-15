f041552c81edd4a8e9559699ac98272f092cbc98
Fahim Mohammed <mohammedf2606@gmail.com>
2022-02-15 12:46:11 +0000

    Start on part 2 cluster

--- coursework/adult.py
+++ coursework/adult.py
@@ -1,14 +1,18 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+
 
 # Part 1: Decision Trees with Categorical Attributes
 
 # Return a pandas dataframe containing the data set that needs to be extracted from the data_file.
 # data_file will be populated with the string 'adult.csv'.
+from sklearn.tree import DecisionTreeClassifier
 
 
 def read_csv_1(data_file):
 	df = pd.read_csv(data_file)
-	return df
+	return df.drop(columns="fnlwgt")
 
 
 # Return the number of rows in the pandas dataframe df.
@@ -28,9 +32,7 @@ def missing_values(df):
 
 # Return a list with the columns names containing at least one missing value in the pandas dataframe df.
 def columns_with_missing_values(df):
-	missing_values_cols = df.isnull().sum()
-	print(missing_values_cols)
-	return [x for x in missing_values_cols if x != 0]
+	return list(df.columns[df.isnull().any()])
 
 
 # Return the percentage of instances corresponding to persons whose education level is 
@@ -38,13 +40,15 @@ def columns_with_missing_values(df):
 # in the pandas dataframe df containing the data set in the adult.csv file.
 # For example, if the percentage is 0.21547%, then the function should return 0.216.
 def bachelors_masters_percentage(df):
-	pass
+	education = df['education']
+	bachelors_masters = [x for x in education if x == "Bachelors" or x == "Masters"]
+	return round(len(bachelors_masters)/len(education) * 100, 1)
 
 
 # Return a pandas dataframe (new copy) obtained from the pandas dataframe df 
 # by removing all instances with at least one missing value.
 def data_frame_without_missing_values(df):
-	pass
+	return df.dropna()
 
 
 # Return a pandas dataframe (new copy) from the pandas dataframe df
@@ -52,28 +56,37 @@ def data_frame_without_missing_values(df):
 # The function should not encode the target attribute, and the function's output
 # should not contain the target attribute.
 def one_hot_encoding(df):
-	pass
+	df_temp = df.drop(columns="class")
+	one_hot = OneHotEncoder(handle_unknown='ignore')
+	one_hot_encoded = one_hot.fit_transform(df_temp).toarray()
+	attribute_names = one_hot.get_feature_names_out(column_names(df_temp))
+	one_hot_df = pd.DataFrame(one_hot_encoded, columns=attribute_names)
+	return one_hot_df
 
 
 # Return a pandas series (new copy), from the pandas dataframe df, 
 # containing only one column with the labels of the df instances
 # converted to numeric using label encoding. 
 def label_encoding(df):
-	pass
+	label_enc = LabelEncoder()
+	return pd.DataFrame(label_enc.fit_transform(df['class']), columns=['class'])
 
 
 # Given a training set X_train containing the input attribute values 
 # and labels y_train for the training instances,
 # build a decision tree and use it to predict labels for X_train. 
 # Return a pandas series with the predicted values. 
-def dt_predict(X_train,y_train):
-	pass
+def dt_predict(X_train, y_train):
+	clf = DecisionTreeClassifier(random_state=0)
+	clf.fit(X_train, y_train)
+	return clf.predict(X_train)
 
 
 # Given a pandas series y_pred with the predicted labels and a pandas series y_true with the true labels,
 # compute the error rate of the classifier that produced y_pred.  
 def dt_error_rate(y_pred, y_true):
-	pass
+	correct_predictions = [i == j for i, j in zip(y_pred, y_true)]
+	return len(correct_predictions)/len(y_true) * 100
 
 
 if __name__ == "__main__":
@@ -82,3 +95,10 @@ if __name__ == "__main__":
 	print(column_names(df))
 	print(missing_values(df))
 	print(columns_with_missing_values(df))
+	print(bachelors_masters_percentage(df))
+	print(len(data_frame_without_missing_values(df)))
+	x_train = one_hot_encoding(df)
+	y_train = label_encoding(df)
+	y_pred = dt_predict(x_train, y_train)
+	print(dt_error_rate(y_pred, y_train))
+
--- coursework/wholesale_customers.py
+++ coursework/wholesale_customers.py
@@ -2,44 +2,55 @@
 
 # Return a pandas dataframe containing the data set that needs to be extracted from the data_file.
 # data_file will be populated with the string 'wholesale_customers.csv'.
+import pandas as pd
+
+
 def read_csv_2(data_file):
-	pass
+    df = pd.read_csv(data_file)
+    return df.drop(columns=["Channel", "Region"])
+
 
 # Return a pandas dataframe with summary statistics of the data.
 # Namely, 'mean', 'std' (standard deviation), 'min', and 'max' for each attribute.
 # These strings index the new dataframe columns. 
 # Each row should correspond to an attribute in the original data and be indexed with the attribute name.
 def summary_statistics(df):
-	pass
+    mean = round(df.mean())
+    std = round(df.std())
 
 # Given a dataframe df with numeric values, return a dataframe (new copy)
 # where each attribute value is subtracted by the mean and then divided by the
 # standard deviation for that attribute.
 def standardize(df):
-	pass
+    pass
+
 
 # Given a dataframe df and a number of clusters k, return a pandas series y
 # specifying an assignment of instances to clusters, using kmeans.
 # y should contain values in the set {0,1,...,k-1}.
 def kmeans(df, k):
-	pass
+    pass
+
 
 # Given a dataframe df and a number of clusters k, return a pandas series y
 # specifying an assignment of instances to clusters, using kmeans++.
 # y should contain values from the set {0,1,...,k-1}.
 def kmeans_plus(df, k):
-	pass
+    pass
+
 
 # Given a dataframe df and a number of clusters k, return a pandas series y
 # specifying an assignment of instances to clusters, using agglomerative hierarchical clustering.
 # y should contain values from the set {0,1,...,k-1}.
 def agglomerative(df, k):
-	pass
+    pass
+
 
 # Given a data set X and an assignment to clusters y
 # return the Solhouette score of the clustering.
-def clustering_score(X,y):
-	pass
+def clustering_score(X, y):
+    pass
+
 
 # Perform the cluster evaluation described in the coursework description.
 # Given the dataframe df with the data to be clustered,
@@ -50,15 +61,21 @@ def clustering_score(X,y):
 # 'k': the number of clusters produced,
 # 'Silhouette Score': for evaluating the resulting set of clusters.
 def cluster_evaluation(df):
-	pass
+    pass
+
 
 # Given the performance evaluation dataframe produced by the cluster_evaluation function,
 # return the best computed Silhouette score.
 def best_clustering_score(rdf):
-	pass
+    pass
+
 
 # Run some clustering algorithm of your choice with k=3 and generate a scatter plot for each pair of attributes.
 # Data points in different clusters should appear with different colors.
 def scatter_plots(df):
-	pass
+    pass
 
+
+if __name__ == "__main__":
+    df = read_csv_2('./data/wholesale_customers.csv')
+    print(summary_statistics(df))
